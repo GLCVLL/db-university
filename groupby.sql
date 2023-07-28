c60671b19501b97c395341bdeade9a567e1ea929
GLCVLL <g.ioele96@outlook.it>
2023-07-28 16:21:54 +0200

    added teachers same address

--- groupby.sql
+++ groupby.sql
@@ -3,3 +3,12 @@
 SELECT YEAR(enrolment_date) AS AnnoIscrizione, COUNT(*) AS NumeroIscritti 
 FROM `students`
 GROUP BY YEAR(enrolment_date);
+
+2 --Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT office_address, COUNT(*) AS NumeroInsegnanti 
+FROM `teachers`
+GROUP BY office_address
+HAVING COUNT(*) > 1;
+
+3 -- Calcolare la media dei voti di ogni appello d'esame
