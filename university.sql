b50a41ed1a1a8c9c14538970af97e8999384fec3
GLCVLL <g.ioele96@outlook.it>
2023-07-28 15:41:04 +0200

    added exams in pm time on 2020/06/20

--- university.sql
+++ university.sql
@@ -1,24 +1,31 @@
---Selezionare tutti gli studenti nati nel 1990
+1 --Selezionare tutti gli studenti nati nel 1990
 
 SELECT * 
 FROM `students`
 WHERE YEAR(date_of_birth) = 1990;
 
---Selezionare tutti i corsi che valgono più di 10 credit
+2 --Selezionare tutti i corsi che valgono più di 10 credit
 
 SELECT * 
 FROM `courses`
 WHERE `cfu` > 10;
 
---Selezionare tutti gli studenti che hanno più di 30 anni
+3 --Selezionare tutti gli studenti che hanno più di 30 anni
 
 SELECT * 
 FROM `students`
 WHERE DATEDIFF(CURDATE(), date_of_birth) / 365.25 > 30;
 
---Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea
+4 --Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea
 
 SELECT * 
 FROM `courses`
 WHERE `period` = 'I semestre'
 AND `year` = '1';
+
+5 --Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020
+
+SELECT * 
+FROM `exams`
+WHERE `date` = '2020-06-20'
+AND `hour` >= '14:00:00';
