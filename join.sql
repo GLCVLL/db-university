353fa9189375e2c939b2b5961703ffd9eeac1443
GLCVLL <g.ioele96@outlook.it>
2023-07-31 16:17:29 +0200

    added students department and courses

--- join.sql
+++ join.sql
@@ -19,3 +19,11 @@ FROM courses AS c
 JOIN course_teacher AS ct ON c.id = ct.course_id
 JOIN teachers AS t ON ct.teacher_id = t.id
 WHERE t.id = 44;
+
+4 --Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT s.name AS nome_studente, s.surname AS cognome_studente, d.name AS corso_di_laurea, dp.name AS dipartimento
+FROM students AS s
+JOIN degrees AS d ON s.degree_id = d.id
+JOIN departments AS dp ON d.department_id = dp.id
+ORDER BY s.surname, s.name;
