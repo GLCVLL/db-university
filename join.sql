beea1c0beecc6d2a275deb20587befb8c486aeec
GLCVLL <g.ioele96@outlook.it>
2023-07-31 15:59:11 +0200

    added department

--- join.sql
+++ join.sql
@@ -1,8 +1,15 @@
 1 -- Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 
 SELECT s.id, s.name, s.surname
-FROM students s
-JOIN degrees d ON s.degree_id = d.id
+FROM students AS s
+JOIN degrees AS d ON s.degree_id = d.id
 WHERE d.name = 'Corso di Laurea in Economia';
 
 2 -- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+SELECT dep.name AS nome_dipartimento
+FROM degrees AS deg
+JOIN departments AS dep ON deg.department_id = dep.id
+WHERE dep.name = 'Dipartimento di Neuroscienze';
+
+3 --Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
